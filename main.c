c0bc8adcfa3cdd5983ee3dc316391bca6f45c0e5
Shane Harding <sharding@tcd.ie>
2015-03-19 14:18:50 +0000

    blah

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<mpi.h>
+#define pi 3.14159265359
 
 int main(){
 
+  int num_cars, i;
+  double *theta, *omega, *omega_bar, a;
+
+  num_cars=20;
+
+  //for(i=0;i<num_cars;i++){
+    printf("%lf\n", pi);
+    //}
 
 }
